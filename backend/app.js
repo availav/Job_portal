d88b5c66348145b29f95aeb7900ef495f9c3e9d3
Vishal <vishaldubey8130@gmail.com>
2024-12-05 23:30:57 +0530

    starting day

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import {config} from 'dotenv';
+import cors from 'cors'
+
+
+const app = express();
+config({path:"./config/config.env"});
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+  
+
+export default app;
